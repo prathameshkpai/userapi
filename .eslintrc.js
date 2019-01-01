fa8c43b7e8051b6b03f91e4e41993b8fab3e3e1a
prathameshkpai <prathameshkow@gmail.com>
2019-01-01 09:09:43 +0530

    Added all Http methods to index file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,28 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2016
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
 };
--- index.js
+++ index.js
@@ -7,14 +7,58 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/details');
+mongoose.Promise = global.Promise;
+
+let Customers = new mongoose.Schema({
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    country: { type: String, required: true }
+});
+
+let customers =mongoose.model('customers',Customers);
 
 app.use(bodyParser.json());
 
 app.get('/sample', (req, res) => {
-    return res.json({ message: 'success' });
+    return res.send('GOt it');
 });
 
+app.post('/sample', (req, res) => {
+    let data = new customers(req.body);
+    data.save().then(() => {
+        return res.send('Inserted');        
+    }).catch((err) => {
+        return res.send('error' + err);
+    });
+});
+
+app.put('/sample', (req, res) => {
+    customers.findOneAndUpdate({ name: req.body.name }, req.body, { new: true }, (err, doc) => {
+        if (!err) {
+            return res.send('updated');
+        } else {
+            return res.send('update failed');
+        }
+    }
+    );});
+
+app.delete('/sample/:id', (req, res) => {
+    customers.findByIdAndRemove(req.params.id, (ee,doc) =>{
+        if(!ee){
+            return res.send('Deleted');
+        }else{
+            return res.send('error' + ee);
+        }
+    });    
+});
+
+
 app.listen(4000, '0.0.0.0', () => {
+    // eslint-disable-next-line no-console
     console.log('Server started successfully!');
 });
